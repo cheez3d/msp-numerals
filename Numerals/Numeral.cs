7e5909bc40b319632f5ed5565e43b0efbccc7217
Andrei Stoian <andrei.st03@gmail.com>
2019-01-10 21:49:55 +0200

    Add project files.

---
+++ Numerals/Numeral.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Text;
+
+
+namespace Numerals
+{
+    class Numeral
+    {
+        private static readonly int RomanNumeralLimit = 3999;
+        private static readonly char[][] RomanNumeralLT;
+
+        static Numeral()
+        {
+            RomanNumeralLT = new char[4][];
+            
+            RomanNumeralLT[0] = new char[2] { 'I', 'V' };
+            RomanNumeralLT[1] = new char[2] { 'X', 'L' };
+            RomanNumeralLT[2] = new char[2] { 'C', 'D' };
+            RomanNumeralLT[3] = new char[1] { 'M',     };
+        }
+
+
+        private int _number;
+
+        private string _arabic;
+        private string _roman;
+
+
+        public int Number {
+            get { return _number; }
+
+            set
+            {
+                _arabic = null;
+                _roman = null;
+
+                _number = value;
+            }
+        }
+
+        public string Arabic
+        {
+            get
+            {
+                if (null != _arabic) { return _arabic; }
+
+                _arabic = Convert.ToString(_number);
+
+                return _arabic;
+            }
+        }
+
+        public string Roman
+        {
+            get
+            {
+                if (null != _roman) { return _roman; }
+                if ((_number <= 0) || !(_number <= RomanNumeralLimit)) { return null; }
+
+                StringBuilder stringBuilder = new StringBuilder();
+
+                int digitCount = (int)Math.Floor(Math.Log10(Math.Abs(_number)) + 1); // number is never 0 here
+
+                // traverse number by digit from left to rigth (MSD to LSD)
+                for (int digitIndex = digitCount-1; digitIndex >= 0; --digitIndex)
+                {
+                    int digit = Math.Abs(_number) / (int)Math.Pow(10, digitIndex) % 10;
+                    if (0 == digit) { continue; }
+
+                    if ((1 <= digit) && (digit <= 3))
+                    {
+                        stringBuilder.Append(RomanNumeralLT[digitIndex][0], digit);
+                    }
+                    else if (4 == digit)
+                    {
+                        stringBuilder.Append(RomanNumeralLT[digitIndex][0]);
+                        stringBuilder.Append(RomanNumeralLT[digitIndex][1]);
+                    }
+                    else if ((5 <= digit) && (digit <= 8))
+                    {
+                        stringBuilder.Append(RomanNumeralLT[digitIndex][1]);
+                        stringBuilder.Append(RomanNumeralLT[digitIndex][0], digit % 5);
+                    }
+                    else if (9 == digit)
+                    {
+                        stringBuilder.Append(RomanNumeralLT[digitIndex][0]);
+                        stringBuilder.Append(RomanNumeralLT[digitIndex+1][0]);
+                    }
+                }
+
+                _roman = stringBuilder.ToString();
+
+                return _roman;
+            }
+        }
+    }
+}
---
+++ Numerals/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.IO;
+
+
+namespace Numerals
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("ARABIC TO ROMAN NUMERAL CONVERTER");
+            Console.WriteLine();
+
+
+            Console.WriteLine("Input file name to read from or press ENTER for manual entry.");
+            string inFileName = Console.ReadLine();
+
+            TextReader textReader;
+
+            if (!String.IsNullOrEmpty(inFileName))
+            {
+                try { textReader = File.OpenText(inFileName); }
+                catch {
+                    Console.WriteLine("Could not open file for reading.");
+
+                    textReader = Console.In;
+                }
+            }
+            else { textReader = Console.In; }
+            Console.WriteLine();
+
+
+            Console.WriteLine("Input file name to write to or press ENTER to skip writing to file.");
+            string outFileName = Console.ReadLine();
+
+            TextWriter textWriter;
+
+            if (!String.IsNullOrEmpty(outFileName))
+            {
+                try { textWriter = File.CreateText(outFileName); }
+                catch {
+                    textWriter = null;
+                    Console.WriteLine("Could not open file for writing.");
+                }
+            }
+            else
+            {
+                textWriter = null;
+                Console.WriteLine("Skipping writing to file.");
+            }
+            Console.WriteLine();
+
+
+            if (textReader == Console.In)
+            {
+                Console.WriteLine("Input numbers to be converted.");
+                Console.WriteLine("When done, input EOF (Ctrl+Z).");
+                Console.WriteLine();
+            }
+
+            Numeral numeral = new Numeral();
+            
+            while (true)
+            {
+                string line = textReader.ReadLine();
+                if (null == line) {
+                    if (textReader == Console.In) { Console.WriteLine(); }
+
+                    break;
+                }
+                
+                if (textReader != Console.In) { Console.Write(line + " -> "); }
+
+                if (!System.Int32.TryParse(line, out int number))
+                {
+                    Console.WriteLine("Input is malformed or too large.");
+                    goto finally_;
+                }
+
+                numeral.Number = number;
+
+                if (null == numeral.Roman)
+                {
+                    Console.WriteLine("Number cannot be converted.");
+                    goto finally_;
+                }
+
+                if (null != textWriter) { textWriter.WriteLine(numeral.Roman); } 
+                Console.WriteLine(numeral.Roman);
+
+            finally_:
+                Console.WriteLine();
+            }
+
+            
+            if (null != textWriter)
+            {
+                textWriter.Flush();
+                Console.WriteLine("All conversions written to file.");
+                Console.WriteLine();
+            }
+
+            Console.Write("Press any key to continue... ");
+            Console.ReadKey();
+        }
+    }
+}
