7e105816c0d74d4302b1351c3221eaa11537ce1d
Andrei Stoian <andrei.st03@gmail.com>
2019-02-19 19:49:44 +0200

    Code refactorization

--- Numerals/Numeral.cs
+++ Numerals/Numeral.cs
@@ -6,19 +6,20 @@ namespace Numerals
 {
     class Numeral
     {
-        private static readonly int RomanNumeralLimit = 3999;
-        private static readonly char[][] RomanNumeralLT; // lookup table for conversion
+        public static readonly int RomanLimitLower = 1;
+        public static readonly int RomanLimitUpper = 3999;
+
+        private static readonly char[][] RomanSymbolTable; // symbol table for conversion
 
         static Numeral()
         {
-            // initialize lookup table
+            // initialize symbol table
+            RomanSymbolTable = new char[4][];
 
-            RomanNumeralLT = new char[4][];
-            
-            RomanNumeralLT[0] = new char[2] { 'I', 'V' };
-            RomanNumeralLT[1] = new char[2] { 'X', 'L' };
-            RomanNumeralLT[2] = new char[2] { 'C', 'D' };
-            RomanNumeralLT[3] = new char[1] { 'M',     };
+            RomanSymbolTable[0] = new char[2] { 'I', 'V' };
+            RomanSymbolTable[1] = new char[2] { 'X', 'L' };
+            RomanSymbolTable[2] = new char[2] { 'C', 'D' };
+            RomanSymbolTable[3] = new char[1] { 'M',     };
         }
 
 
@@ -56,8 +57,12 @@ namespace Numerals
         {
             get
             {
-                if (null != _roman) { return _roman; }
-                if ((_number <= 0) || !(_number <= RomanNumeralLimit)) { return null; }
+                if (null != _roman) {
+                    return _roman;
+                }
+                if ((_number < RomanLimitLower) || (_number > RomanLimitUpper)) {
+                    return null;
+                }
 
                 StringBuilder stringBuilder = new StringBuilder();
 
@@ -67,26 +72,28 @@ namespace Numerals
                 for (int digitIndex = digitCount-1; digitIndex >= 0; --digitIndex)
                 {
                     int digit = Math.Abs(_number) / (int)Math.Pow(10, digitIndex) % 10;
-                    if (0 == digit) { continue; }
+                    if (0 == digit) {
+                        continue;
+                    }
 
                     if ((1 <= digit) && (digit <= 3))
                     {
-                        stringBuilder.Append(RomanNumeralLT[digitIndex][0], digit);
+                        stringBuilder.Append(RomanSymbolTable[digitIndex][0], digit);
                     }
                     else if (4 == digit)
                     {
-                        stringBuilder.Append(RomanNumeralLT[digitIndex][0]);
-                        stringBuilder.Append(RomanNumeralLT[digitIndex][1]);
+                        stringBuilder.Append(RomanSymbolTable[digitIndex][0]);
+                        stringBuilder.Append(RomanSymbolTable[digitIndex][1]);
                     }
                     else if ((5 <= digit) && (digit <= 8))
                     {
-                        stringBuilder.Append(RomanNumeralLT[digitIndex][1]);
-                        stringBuilder.Append(RomanNumeralLT[digitIndex][0], digit % 5);
+                        stringBuilder.Append(RomanSymbolTable[digitIndex][1]);
+                        stringBuilder.Append(RomanSymbolTable[digitIndex][0], digit % 5);
                     }
                     else if (9 == digit)
                     {
-                        stringBuilder.Append(RomanNumeralLT[digitIndex][0]);
-                        stringBuilder.Append(RomanNumeralLT[digitIndex+1][0]);
+                        stringBuilder.Append(RomanSymbolTable[digitIndex][0]);
+                        stringBuilder.Append(RomanSymbolTable[digitIndex+1][0]);
                     }
                 }
 
--- Numerals/Program.cs
+++ Numerals/Program.cs
@@ -6,6 +6,10 @@ namespace Numerals
 {
     class Program
     {
+        private static readonly TextReader TextReaderDefault = Console.In;
+        private static readonly TextWriter TextWriterDefault = Console.Out;
+
+
         static void Main()
         {
             Console.WriteLine("ARABIC TO ROMAN NUMERAL CONVERTER");
@@ -13,42 +17,55 @@ namespace Numerals
 
 
             Console.WriteLine("Input file name to read from or press ENTER for manual entry.");
-            string inFileName = Console.ReadLine();
+            string inputFileName = Console.ReadLine();
 
             TextReader textReader;
-
-            if (!String.IsNullOrEmpty(inFileName))
+            if (!String.IsNullOrEmpty(inputFileName))
             {
-                try { textReader = File.OpenText(inFileName); }
-                catch {
+                try
+                {
+                    textReader = File.OpenText(inputFileName);
+                }
+                catch
+                {
+                    textReader = TextReaderDefault;
+
                     Console.WriteLine("Could not open file for reading.");
-
-                    textReader = Console.In;
                 }
-            }
-            else { textReader = Console.In; }
-            Console.WriteLine();
 
-
-            Console.WriteLine("Input file name to write to or press ENTER to skip writing to file.");
-            string outFileName = Console.ReadLine();
-
-            TextWriter textWriter;
-
-            if (!String.IsNullOrEmpty(outFileName))
-            {
-                try { textWriter = File.CreateText(outFileName); }
-                catch {
-                    textWriter = null;
-                    Console.WriteLine("Could not open file for writing.");
-                }
+                Console.WriteLine();
             }
             else
             {
-                textWriter = null;
-                Console.WriteLine("Skipping writing to file.");
+                textReader = TextReaderDefault;
             }
-            Console.WriteLine();
+
+            Console.WriteLine("Input file name to write to or press ENTER to skip writing to file.");
+            string outputFileName = Console.ReadLine();
+
+            TextWriter textWriter;
+            if (!String.IsNullOrEmpty(outputFileName))
+            {
+                try
+                {
+                    textWriter = File.CreateText(outputFileName);
+                }
+                catch
+                {
+                    textWriter = TextWriterDefault;
+
+                    Console.WriteLine("Could not open file for writing.");
+                }
+
+                Console.WriteLine();
+            }
+            else
+            {
+                textWriter = TextWriterDefault;
+
+                Console.WriteLine("Skipping writing to file.");
+                Console.WriteLine();
+            }
 
 
             if (textReader == Console.In)
@@ -64,17 +81,24 @@ namespace Numerals
             {
                 string line = textReader.ReadLine();
                 if (null == line) {
-                    if (textReader == Console.In) { Console.WriteLine(); }
+                    if (textReader == Console.In)
+                    {
+                        Console.WriteLine();
+                    }
 
                     break;
                 }
                 
-                if (textReader != Console.In) { Console.Write(line + " -> "); }
+                if (textReader != Console.In)
+                {
+                    Console.Write(line + " -> ");
+                }
 
                 if (!System.Int32.TryParse(line, out int number))
                 {
                     Console.WriteLine("Input is malformed or too large.");
-                    goto finally_;
+
+                    goto end;
                 }
 
                 numeral.Number = number;
@@ -82,21 +106,30 @@ namespace Numerals
                 if (null == numeral.Roman)
                 {
                     Console.WriteLine("Number cannot be converted.");
-                    goto finally_;
+                    Console.WriteLine($"Only numbers between {Numeral.RomanLimitLower} and {Numeral.RomanLimitUpper} can be converted.");
+
+                    goto end;
                 }
 
-                if (null != textWriter) { textWriter.WriteLine(numeral.Roman); } 
                 Console.WriteLine(numeral.Roman);
+                if (textWriter != Console.Out)
+                {
+                    textWriter.WriteLine(numeral.Roman);
+                }
 
-            finally_:
+            end:
                 Console.WriteLine();
             }
-            textReader.Close();
 
+            if (textReader != Console.In)
+            {
+                textReader.Close();
+            }
 
-            if (null != textWriter)
+            if (textWriter != Console.Out)
             {
                 textWriter.Close();
+
                 Console.WriteLine("All conversions written to file.");
                 Console.WriteLine();
             }
